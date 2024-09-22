cd61715e147efd52389939ae5802649791a96dbb
MAN4647 <nagvekar98mayuresh@gmail.com>
2024-09-23 02:05:36 +0530

    created the ship and refactor the code

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,30 +1,39 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,500))
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        self.bg_color = (230,230,230)
-
+        self.ship = Ship(self)
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            #Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            #Redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            #Make most recently drawn screen visible
-            pygame.display.flip()
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        #Make most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == "__main__":
     #make game instance and run the game.
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all settings of Alien Invasion"""
+
+    def __init__(self):
+        """initialize the game settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 500
+        self.bg_color = (230,230,230)
+
